029a097c5768f8bd67a9b6ed2c1117554cdd2997
nacindocom <nacindo.com@gmail.com>
2026-03-31 19:30:49 +0700

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -53,7 +53,7 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale} data-theme="light" data-scroll-behavior="smooth">
+    <html lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'} data-theme="light" data-scroll-behavior="smooth">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NextIntlClientProvider messages={messages}>
           {children}
